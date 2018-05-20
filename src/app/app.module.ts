68e7d6cd18d1df556f80348e2167d8cb21822806
varunteri@yahoo.in <you@example.com>
2018-05-21 00:11:50 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
+import { UserCartComponent } from './container/user-cart/user-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent,
+    UserCartComponent
   ],
   imports: [
     BrowserModule
