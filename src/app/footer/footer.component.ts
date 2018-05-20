6750c82f54d2f04dba7bcefa4b212530d64fda13
varunteri@yahoo.in <you@example.com>
2018-05-21 00:43:00 +0530

    initial commit

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Foot } from '../foot';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  foot: Foot = {
+    id: 1,
+    name: 'Varun'
+  };
 
   constructor() { }
 
